e72212a3a98eaf1ff8f776091cfbfc90a23fc8fb
AsmitaTagad <asmitatagad07@gmail.com>
2023-08-01 08:28:46 +0530

    created Api application

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const http = require("http");
 const fs = require("fs");
+const { info } = require("console");
+const { platform } = require("os");
 // const url=require("url")
 
 const server = http.createServer((req, res) => {
-  const newData = `${Date.now()}: ${
-    req.url
-  } get info on which page user are!\n`;
-  fs.appendFile("serverNotes.txt", newData, (err, data) => {
-    //here url provide us the routing path
-    switch (req.url) {
-      case "/":
-        res.end("Home page");
-        break;
+  const path = req.url;
+  const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+  const infoData = JSON.parse(data);
 
-      case "/about":
-        res.end("i am Asmita..");
-        break;
-
-      default:
-        res.end("404 Page Not found");
-    }
-  });
+  if (path === "/" || path === "/content") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end("<h2>This is sample!</h2>");
+  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.end("404 Page Not found");
+  }
 });
 
 server.listen(8000, () => {
